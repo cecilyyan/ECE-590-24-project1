d752834df38797719d6b8fe5464856117a270fa6
cecilyyan <454585636@qq.com>
2020-02-02 17:17:14 -0500

    yes

--- main.py
+++ main.py
@@ -12,6 +12,13 @@ def hello():
 def name(value):
     val = {"value": value}
     return jsonify(val)
+@app.route('/my')
+def html():
+    """Returns some custom HTML"""
+    return """
+    <title>HTML Page</title>
+    <p><b>Hello!!! Welcome to my HTML page : ) </b></p>
+    """
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
