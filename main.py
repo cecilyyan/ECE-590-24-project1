c3471d09bb4d385a9c9e361f18020c913b702771
cecilyyan <454585636@qq.com>
2020-02-02 17:28:35 -0500

    changes of main

--- main.py
+++ main.py
@@ -3,15 +3,19 @@ from flask import jsonify
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return 'Hello I like to make AI Apps!!!!!!'
+    return 'Hello I like to make Apps!!!!!!'
+
 
 @app.route('/name/<value>')
 def name(value):
     val = {"value": value}
     return jsonify(val)
+
+
 @app.route('/my')
 def html():
     """Returns some custom HTML"""
@@ -19,6 +23,7 @@ def html():
     <title>HTML Page</title>
     <p><b>Hello!!! Welcome to my HTML page : ) </b></p>
     """
+
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
